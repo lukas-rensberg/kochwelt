3a4f60903612d1ae8edef770fa1223eab3d410d6
lukas@lrlp.dev <lukas@lrlp.dev>
2025-08-19 23:48:40 +0200

    feat: Mobile Navigation hinzugefügt und Burger-Menü-Funktionalität implementiert 👍

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,3 +3,13 @@ document.addEventListener("DOMContentLoaded",function(e){
     const DATE = new Date().getFullYear();
     DATE_ELEMENT.innerHTML = DATE;
 })
+
+function toggleMobileNavigation() {
+    const mobileNav = document.getElementById("mobile-navigation");
+    const burgerMenu = document.querySelector(".burger-menu");
+    
+    if (mobileNav) {
+        mobileNav.classList.toggle("active");
+        burgerMenu.classList.toggle("active");
+    }
+}
