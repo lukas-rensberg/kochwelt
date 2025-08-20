5b2f9bb35d520deb52469edfa2b3138f41c547f7
lukas@lrlp.dev <lukas@lrlp.dev>
2025-08-20 12:02:59 +0200

    feat(navigation): update "recipe of the day" link and add random recipe selection

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded",function(e){
+let recipeOfTheDay;
+
+document.addEventListener("DOMContentLoaded", function (e) {
     const DATE_ELEMENT = document.getElementById("year");
     const DATE = new Date().getFullYear();
     DATE_ELEMENT.innerHTML = DATE;
@@ -11,5 +13,21 @@ function toggleMobileNavigation() {
     if (mobileNav) {
         mobileNav.classList.toggle("active");
         burgerMenu.classList.toggle("active");
+    }
+}
+
+function openRecipeOfTheDay() {
+    recipeOfTheDay = getRandomRecipe();
+    window.location.href = recipeOfTheDay;
+}
+
+function getRandomRecipe() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+        return "/rezept-rensberg/";
+    } else if (randomNumber > 0.5 && randomNumber < 0.8) {
+        return "/Rezept-Keil/";
+    } else {
+        return "/rezept-kleppmann/";
     }
 }
